2505f16bf3e2f32a20de65c08e33ae51b0a8d918
NickBenevento <nbenevento99@gmail.com>
2020-11-22 23:36:52 -0500

    final submission

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # nlp_assignment_3
 
 ## Run Instructions
+1. Ensure that sklearn is installed. 
 1. Run the command `pip install -r reqs.txt` to install required libraries.
 1. Run the homework script: `python homework3.py`
 
--- homework3.py
+++ homework3.py
@@ -1,19 +1,19 @@
-from typing import List
-import numpy.random as rng
 import gensim
 import gzip
-from gensim.models import Word2Vec
-from nltk.probability import FreqDist
-from nltk.corpus import brown
-from nltk.corpus import stopwords
-import nltk
-import numpy as np
-from pandas import DataFrame
-# import nltk
 import matplotlib.pyplot as plt
 import pandas
 import re
-import sklearn
+
+from typing import List
+from gensim.models import Word2Vec
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from pandas import DataFrame
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
 stop_words = set(stopwords.words('english'))
@@ -26,8 +26,8 @@ def parse_file(file_name: str) -> DataFrame:
     return data
 
 
-def clean_data(tweets) -> List[List[str]]:
-    cleaned_tweets: List[List[str]] = []
+def clean_data(tweets) -> List[str]:
+    cleaned_tweets: List[str] = []
 
     for i, tweet in enumerate(tweets):
         clean = tweet.strip()
@@ -41,10 +41,13 @@ def clean_data(tweets) -> List[List[str]]:
         clean = re.sub(r'[^\'a-zA-Z0-9.\s]', '', clean)
 
         clean = clean.strip().lower()
+
         clean = gensim.utils.simple_preprocess(clean)
+
         # Remove stopwords
         clean = [word for word in clean if word not in stop_words]
 
+        clean = ' '.join(clean)
         cleaned_tweets.append(clean)
 
     return cleaned_tweets
@@ -56,17 +59,10 @@ def part0(file_name: str) -> DataFrame:
 
 
 def part1(data: DataFrame):
-    # print(data['tweet'])
     model: Word2Vec = Word2Vec(data['tweet'], size=150, window=10, min_count=2, workers=10)
     model.train(data['tweet'], total_examples=len(data['tweet']), epochs=10)
-    part1_q1(model)
+    part1_q1_q3()
     part1_q2(data)
-    part1_q3()
-
-
-def part1_q1(model: Word2Vec):
-    print(f"Most similar to 'hate': {model.wv.most_similar(positive=['hate'])[:1]}")
-    print(f"Most similar to 'like': {model.wv.most_similar(positive=['like'])[:1]}")
 
 
 def part1_q2(data: DataFrame):
@@ -96,12 +92,16 @@ def part1_q2(data: DataFrame):
     plt.show()
 
 
-def part1_q3():
+def part1_q1_q3():
     imdb = list(read_imdb('imdb.txt.gz'))
     print('Training...')
     model = gensim.models.Word2Vec(imdb, size=150, window=10, min_count=2, workers=10)
     model.train(imdb, total_examples=len(imdb), epochs=10)
     print('Done training')
+
+    print(f"Most similar to 'hate': {model.wv.most_similar(positive=['hate'])[:1]}")
+    print(f"Most similar to 'like': {model.wv.most_similar(positive=['like'])[:1]}")
+
     print(model.wv.most_similar(positive=['king', 'woman'], negative=['man'])[:2])
     print(f"Analogy for love + life - hate: {model.wv.most_similar(positive=['love', 'life'], negative=['hate'])[:1]}")
 
@@ -111,49 +111,56 @@ def read_imdb(input_file: str) -> List[str]:
         for i, line in enumerate(f):
             if i % 10000 == 0:
                 print("read {0} reviews".format(i))
-                # do some pre-processing and return a list of words for each review text
+            # do some pre-processing and return a list of words for each review text
             yield gensim.utils.simple_preprocess(line)
-
-
-def test_accuracy(model, test: DataFrame):
-    print('Testing Accuracy for Naive Bayes model:')
-    correct = 0
-    for i, tweet in enumerate(test['tweet']):
-        predicted = model.predict(tweet)
-        if predicted == test['label'][i]:
-            correct += 1
-
-    accuracy: float = correct / len(test['tweet'])
-    print(f'Accuracy: {accuracy}')
-    return
 
 
 def part2(train: DataFrame):
     print('Part 2:')
     part2_q1(train)
-    test_tweets = parse_file('test.csv')
-    # Naive Bayes with add-1 smoothing using binary bag-of-ngrams features
-    clf = MultinomialNB()
-    x = np.vstack(train['tweet'])
-    # y = train['label'].astype(np.float).to_numpy()
-    y = np.array(train['label']).astype(np.int)
-    print(y)
-    print(type(y[0]))
-
-    clf.fit(x, y)
-    # clf.fit(train['tweet'], train['label'])
-    # test_accuracy(clf, test_tweets)
+    part2_q2(train)
+    part2_q3(train)
     return
 
 
-def part2_q1(model: DataFrame):
-    return
+def part2_q1(data: DataFrame):
+    model = MultinomialNB(alpha=1)
+    vectorizer = CountVectorizer(ngram_range=(1, 2), binary=True)
+    apply_model(model, vectorizer, data)
+
+
+def part2_q2(data: DataFrame):
+    model = LogisticRegression()
+    vectorizer = CountVectorizer(binary=True)
+    apply_model(model, vectorizer, data)
+
+
+def part2_q3(data: DataFrame):
+    model = LogisticRegression(multi_class='ovr')
+    vectorizer = CountVectorizer(ngram_range=(1, 2), binary=True)
+    apply_model(model, vectorizer, data)
+
+
+def apply_model(model, vectorizer: CountVectorizer, data: DataFrame):
+    train_data = vectorizer.fit_transform(data['tweet']).toarray()
+    train_label = data['label'].to_numpy()
+
+    data_train, data_test, label_train, label_test = train_test_split(train_data, train_label, test_size=0.238310853)
+    label_test = label_test.ravel()
+    model.fit(data_train, label_train)
+
+    pred_labels = model.predict(data_test)
+    print(confusion_matrix(label_test, pred_labels))
+    print(classification_report(label_test, pred_labels))
+    print(accuracy_score(label_test, pred_labels))
+    f1 = f1_score(label_test, pred_labels)
+    print(f'f1-score: {f1}')
 
 
 def main():
     data: DataFrame = part0('train.csv')
     part1(data)
-    # part2(data)
+    part2(data)
 
 
 main()
